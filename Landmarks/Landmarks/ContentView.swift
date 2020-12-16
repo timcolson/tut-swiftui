48a197c255f1d63ae46f39032e996eb994909c24
Tim Colson <timcolson@42clients.com>
2020-12-16 14:35:35 -0700

    Bring code up to Section 3.9 with padding

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack (alignment: .leading) {
+      Text("Turtle Rock")
+        .font(.title)
+      HStack {
+        Text("Joshua Tree National Park")
+          .font(.subheadline)
+        Spacer()
+        Text("California")
+          .font(.subheadline)
+      }
     }
+    .padding(25)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
