6b3267d5ac822a620b74a479500bfb3eac801ec8
Tim Colson <timcolson@42clients.com>
2020-12-16 23:04:26 -0700

    Compose map, image, and text fields into one VStack

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -8,25 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    VStack (alignment: .leading) {
-      Text("Turtle Rock")
-        .font(.title)
-      HStack {
-        CircleImage()
-        Text("Joshua Tree National Park")
-          .font(.subheadline)
-        Spacer()
-        Text("California")
-          .font(.subheadline)
-      }
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree Nat'l Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.gray)
+
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                
+            }
+            .padding(25)
+            Spacer()
+            
+            
+        }
     }
-    .padding(25)
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        Group {
+            
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewLayout(.fixed(width: 1112, height: 834))
+        }
+        
+    }
+    
 }
