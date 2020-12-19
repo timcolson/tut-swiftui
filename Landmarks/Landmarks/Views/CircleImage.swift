9782389967b5dda046ed45ea6c8fe5080d813746
Tim Colson <timcolson@42clients.com>
2020-12-18 18:11:25 -0700

    Add NavBar/Link and dynamic props for landmarks

--- Landmarks/Landmarks/Views/CircleImage.swift
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -1,8 +1,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(RoundedRectangle(cornerRadius: 42.0))
             .overlay(RoundedRectangle(cornerRadius: 42.0)
                         .stroke(Color.white, lineWidth: 4))
@@ -12,6 +14,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -1,58 +1,14 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Tim Colson on 12/6/20.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack (alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree Nat'l Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.gray)
-
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-                
-            }
-            .padding(25)
-            Spacer()
-            
-            
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            
-            ContentView()
-                .preferredColorScheme(.dark)
-                .previewLayout(.fixed(width: 1112, height: 834))
-        }
-        
+        LandmarkList()
     }
     
 }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
