7ca47cb74c408f3a75bc4a4e471e91a4e302ae68
Tim Colson <timcolson@42clients.com>
2021-01-15 13:51:49 -0700

    include model data in env for profile host preview

--- Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -15,5 +15,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
