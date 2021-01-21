e83cb023bb8f1945f54875a05fc7b9e022128447
Tim Colson <timcolson@42clients.com>
2021-01-20 17:38:46 -0700

    Start integration of UIKit views into SwiftUI
    At this point, the Coordinator has only been created and a list of
    view controllers have been initialized.  No relationships for swiping
    have been established.
    
    It is unclear why a UIKit integration is necessary; perhaps just for
    a learning activity?

--- Landmarks/Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -2,18 +2,44 @@ import SwiftUI
 import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    
     var pages: [Page]
+
+    
+    /* SwiftUI calls this makeCoordinator() method before makeUIViewController(context:),
+     so that you have access to the coordinator object when configuring your view controller.
+    
+     You can use this coordinator to implement common Cocoa patterns,
+     such as delegates, data sources, and responding to user events via target-action.
+
+     */
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
-
+        
         return pageViewController
     }
-
+    
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
     }
+    
+    class Coordinator : NSObject {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            // Q: What does this really do?
+            // A: Converts SwiftUI views into UIKit compatible view controllers.
+            controllers = parent.pages.map {UIHostingController(rootView: $0) }
+        }
+    }
+    
 }
