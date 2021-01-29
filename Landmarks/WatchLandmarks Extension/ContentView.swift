301c792d25832c7d8150bbf2333a56ca5839dc52
Tim Colson <timcolson@42clients.com>
2021-01-29 00:04:37 -0700

    Create watch target, extension, and initial view

---
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Tim Colson on 1/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
