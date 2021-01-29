86f63b570302f921ac76848f5fab2b0ca256c466
Tim Colson <timcolson@42clients.com>
2021-01-29 00:54:31 -0700

    Create link to LandmarkDetail from Category thumb

--- Landmarks/Landmarks/Views/Categories/CategoryItem.swift
+++ Landmarks/Landmarks/Views/Categories/CategoryItem.swift
@@ -10,17 +10,20 @@ import SwiftUI
 struct CategoryItem: View {
     var landmark : Landmark
     var body: some View {
-        VStack(alignment: .leading) {
-            landmark.image
-                .resizable()
-                .frame(width: 100, height: 100)
-                .cornerRadius(5)
-            Text(landmark.name)
-                .font(.system(size:14))
-                .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .leading)
+        NavigationLink(destination:LandmarkDetail(landmark: landmark)) {
+            
+            VStack(alignment: .leading) {
+                landmark.image
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(5)
+                Text(landmark.name)
+                    .font(.system(size:14))
+                    .lineLimit(2)
+                    .frame(width: 100, alignment: .leading)
+            }
+            .padding(.leading, 15)
         }
-        .padding(.leading, 15)
     }
 }
 
