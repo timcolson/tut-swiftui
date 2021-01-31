fd41af9699bb77aea248c3e36092304180c546fb
Tim Colson <timcolson@42clients.com>
2021-01-31 00:10:08 -0700

    Embed landmark details into a scrollview with 40 and 44 preview

--- Landmarks/WatchLandmarks Extension/LandmarkDetail.swift
+++ Landmarks/WatchLandmarks Extension/LandmarkDetail.swift
@@ -10,9 +10,34 @@ struct LandmarkDetail: View {
     }
     
     var body: some View {
-        CircleImage(image: landmark.image.resizable())
-            .scaledToFill()
-//        Text("Hello LM \(landmarkIndex)")
+        
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+                
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+                Divider()
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                Text(landmark.state)
+                    .font(.caption)
+                Divider()
+                MapView(coordinate: landmark.locationCoordinate)
+                    .scaledToFit()
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
     }
 }
 
@@ -22,14 +47,14 @@ struct LandmarkDetail_Previews: PreviewProvider {
         let modelData = ModelData()
         
         return Group {
-                    LandmarkDetail(landmark: modelData.landmarks[0])
-                        .environmentObject(modelData)
-                        .previewDevice("Apple Watch Series 5 - 44mm")
-
-                    LandmarkDetail(landmark: modelData.landmarks[1])
-                        .environmentObject(modelData)
-                        .previewDevice("Apple Watch Series 5 - 40mm")
-                }
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 44mm")
+            
+            LandmarkDetail(landmark: modelData.landmarks[1])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 40mm")
+        }
         
     }
 }
